fb526ff06ed4c4fcca7af52074aa0762cbddf994
naomitzhao <116607148+naomitzhao@users.noreply.github.com>
2024-08-22 14:40:17 -0700

    feat: Init client Next app

---
+++ client/src/app/(pages)/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main id={styles.homeMain}>
+      <h1 id={styles.mainHeader}>study capy</h1>
+    </main>
+  );
+}
