dc1c37253c36f2f88072a5427ec490bc71df2310
naomitzhao <116607148+naomitzhao@users.noreply.github.com>
2024-08-25 23:00:51 -0700

    feat: Add validation to create user and switch user controller responses to JSON

--- server/controllers/usersController.ts
+++ server/controllers/usersController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { createUser, deleteUser, getAllUsers, getUser, updateUser } from '../services/usersService.js';
-// import { body, validationResult } from "express-validator";
+import { body, validationResult } from "express-validator";
 
 /**
  * 
@@ -10,7 +10,10 @@ import { createUser, deleteUser, getAllUsers, getUser, updateUser } from '../ser
 export const getAllUsersController = async (req: Request, res: Response) => {
     try {
         const users = await getAllUsers();
-        res.status(200).send(users);
+        res.status(200).json({
+            success: true,
+            data: users,
+        });
     } catch (e: unknown) {
         if (e instanceof Error){
             res.status(400).json({
@@ -35,7 +38,10 @@ export const getUserController = async (req: Request, res: Response) => {
     try {
         const id: number = parseInt(req.params.id);
         const user = await getUser(id);
-        res.status(200).send(user);
+        res.status(200).json({
+            success: true,
+            data: user,
+        });
     } catch (e: unknown) {
         if (e instanceof Error){
             res.status(400).json({
@@ -51,17 +57,33 @@ export const getUserController = async (req: Request, res: Response) => {
     }
 };
 
+export const validateCreateUser = [
+    body("username").trim().notEmpty().withMessage("Username cannot be empty."),
+    body("email").trim().notEmpty().withMessage("Email cannot be empty."),
+];
+
 /**
  * 
  * @param req Express request
  * @param res Express response
  */
 export const createUserController = async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        // return so no other responses can be sent
+        return res.status(400).json({
+            success: false,
+            message: errors
+        });
+    }
     try {
         const email: string = req.body.email;
         const username: string = req.body.username;
         const user = await createUser(email, username);
-        res.status(200).send(user);
+        res.status(200).json({
+            success: true,
+            data: user,
+        });
     } catch (e: unknown) {
         if (e instanceof Error){
             res.status(400).json({
@@ -86,11 +108,15 @@ export const updateUserController = async (req: Request, res: Response) => {
     try {
         const id: number = parseInt(req.params.id);
         if (req.body.hasOwnProperty("username")) {
-            const user = updateUser(id, "username", req.body.username);
-            res.status(200).send(user);
+            await updateUser(id, "username", req.body.username);
+            res.status(200).json({
+                success: true,
+            });
         } else if (req.body.hasOwnProperty("email")) {
-            const user = updateUser(id, "email", req.body.email);
-            res.status(200).send(user);
+            await updateUser(id, "email", req.body.email);
+            res.status(200).json({
+                success: true,
+            });
         } else {
             throw Error("Can only update username or email.")
         }
@@ -118,7 +144,9 @@ export const deleteUserController = async (req: Request, res: Response) => {
     try {
         const id: number = parseInt(req.params.id);
         await deleteUser(id);
-        res.status(200).send(id);
+        res.status(200).json({
+            success: true
+        });
     } catch (e: unknown) {
         if (e instanceof Error){
             res.status(400).json({
--- server/routes/usersRouter.ts
+++ server/routes/usersRouter.ts
@@ -1,11 +1,17 @@
 import { Router, Request, Response } from "express";
-import { createUserController, deleteUserController, getAllUsersController, getUserController, updateUserController } from "../controllers/usersController.js";
+import { createUserController, 
+    deleteUserController, 
+    getAllUsersController, 
+    getUserController, 
+    updateUserController, 
+    validateCreateUser
+} from "../controllers/usersController.js";
 
 const usersRouter = Router();
 
 usersRouter.get("/", (req: Request, res: Response) => getAllUsersController(req, res));
 usersRouter.get("/:id", (req: Request, res: Response) => getUserController(req, res));
-usersRouter.post("/", (req: Request, res: Response) => createUserController(req, res));
+usersRouter.post("/", validateCreateUser, (req: Request, res: Response) => createUserController(req, res));
 usersRouter.put("/:id", (req: Request, res: Response) => updateUserController(req, res));
 usersRouter.delete("/:id", (req: Request, res: Response) => deleteUserController(req, res));
 
