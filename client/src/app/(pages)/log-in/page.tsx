972dbab205d1a6c6286a6ef2dddcd0f0ad1e42ac
naomitzhao <116607148+naomitzhao@users.noreply.github.com>
2024-09-16 23:10:47 -0700

    feat: Use API for log-in

--- client/src/app/(pages)/log-in/page.tsx
+++ client/src/app/(pages)/log-in/page.tsx
@@ -1,15 +1,38 @@
+"use client";
+
 import Footer from "../../(components)/footer/footer";
 import Header from "../../(components)/header/header";
 import styles from "./page.module.css";
 import Link from "next/link";
 
 export default function Page() {
+    async function handleSubmit(e: Event) {
+        if (!e) {
+            throw new Error("something went wrong :(");
+        }
+        e.preventDefault();
+        console.log(e);
+        console.log(e.target.elements[0].value);
+        console.log(e.target.elements[1].value)
+
+        const result = await fetch("http://localhost:3000/log-in", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username: e.target.elements[0].value,
+                password: e.target.elements[1].value,
+            }),
+        })
+    }
+
     return (
         <div id={styles.page}>
             <Header />
             <div id={styles.content}>
                 <h1>log in</h1>
-                <form id={styles.logInForm}>
+                <form id={styles.logInForm} onSubmit={handleSubmit}>
                     <div id={styles.formInputs}>
                         <input type="text" placeholder="username"/>
                         <input type="password" placeholder="password"/>
